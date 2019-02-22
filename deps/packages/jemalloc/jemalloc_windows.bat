2cc1031bc3b023296d6df6605f462b0610a0d84d
Build Team <build-team@couchbase.com>
2019-02-22 00:29:06 -0800

    jemalloc: Restore Windows build steps
    Change-Id: I7c81e478264f2cdd026d761aba9f66881afd1496

--- deps/packages/jemalloc/jemalloc_windows.bat
+++ deps/packages/jemalloc/jemalloc_windows.bat
@@ -1,4 +1,6 @@
 set INSTALL_DIR=%1
 
-rem Figure out rest later
-exit /B 1
+sh -c "./autogen.sh CC=cl --prefix=%INSTALL_DIR% --with-jemalloc-prefix=je_	--disable-cache-oblivious --disable-zone-allocator --enable-prof"
+
+make build_lib_shared
+make install_lib_shared install_include install_bin
